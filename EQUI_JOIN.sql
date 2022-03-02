733a2486bd7b4686caaa259b1d6549c83be0a123
hankaul <gudgksruf12@naver.com>
2022-03-02 18:50:24 +0900

    SQL200_58

--- EQUI_JOIN.sql
+++ EQUI_JOIN.sql
@@ -1,17 +1,21 @@
-SELECT e.ename, d.loc, e.job, e.deptno
-FROM EMP e, DEPT d
-WHERE e.DEPTNO = d.dept.DEPTNO
+SELECT ENAME, LOC
+FROM EMP e,DEPT d 
+WHERE e.DEPTNO = d.DEPTNO
 
-SELECT e.ename, d.loc, e.job, e.deptno
-FROM EMP e, DEPT d
-WHERE e.DEPTNO = d.dept.DEPTNO
+
+SELECT e.ENAME, d.LOC , e.JOB 
+FROM EMP e, DEPT d 
+WHERE e.DEPTNO = d.DPTNO 
+AND e.job = 'ANALYST'
+
 
 --SELECT ename,loc,job,deptno
 --FROM emp, DEPT
 --WHERE emp.DEPTNO = dept.DEPTNO
 --error : 열의 정의가 애매합니다 
 
+
 --수정한 쿼리문
 SELECT e.ename, d.loc, e.job, e.deptno
 FROM EMP e, DEPT d
-WHERE e.DEPTNO = d.dept.DEPTNO
+WHERE e.DEPTNO = d.DEPTNO
